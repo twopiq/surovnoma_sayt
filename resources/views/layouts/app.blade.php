0819f8f96f30a85cb80cc13096f9d6dd52b84790
twopiq <t.boymurodov@ttyesi.uz>
2026-06-01 10:26:46 +0500

    -logo qo'shildi;

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
         <title>{{ config('app.name', 'RTT Markazi Elektron Murojaatlar Tizimi') }}</title>
 
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600;space-grotesk:500,700&display=swap" rel="stylesheet" />
 
